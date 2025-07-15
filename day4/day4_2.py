e0cc824e1d41e57711ea4b8002b0a9840994ba9d
Andrew Cuonzo <cuonzo.andrew@gmail.com>
2025-07-15 11:00:53 -0400

    fix syntax

--- day4/day4_2.py
+++ day4/day4_2.py
@@ -5,17 +5,11 @@ text = np.genfromtxt("day4/input4.txt", delimiter=1, dtype=str)
 # top left to bottom right horiz
 for i in range(1,SIZE-1,1):
     for j in range(1,SIZE-1,1):
-        is_A = True if text[i][j] == 'A' else False
-        if((text[i+1][j+1] == 'S' and text[i-1][j-1] == 'M') or
-           (text[i+1][j+1] == 'M' and text[i-1][j-1] == 'S')):
-            is_diag1 = True
-        else:
-            is_diag1 = False
-        if((text[i+1][j-1] == 'S' and text[i-1][j+1] == 'M') or
-           (text[i+1][j-1] == 'M' and text[i-1][j+1] == 'S')):
-            is_diag2 = True
-        else:
-            is_diag2 = False
+        is_A = text[i][j] == 'A'
+        is_diag1 = ((text[i+1][j+1] == 'S' and text[i-1][j-1] == 'M') or
+           (text[i+1][j+1] == 'M' and text[i-1][j-1] == 'S'))
+        is_diag2 = ((text[i+1][j-1] == 'S' and text[i-1][j+1] == 'M') or
+           (text[i+1][j-1] == 'M' and text[i-1][j+1] == 'S'))
         if(is_A and is_diag1 and is_diag2):
             count += 1
     
